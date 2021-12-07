667ea8e26b3093ff3769945cdd569c9f10c5a669
ldex <laurent.duveau+git@gmail.com>
2021-12-07 12:02:52 -0500

    Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './shared/admin.component';
+import { ContactComponent } from './shared/contact.component';
+import { ErrorComponent } from './shared/error.component';
+import { HomeComponent } from './shared/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
